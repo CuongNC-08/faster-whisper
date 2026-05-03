9dac46d0112a6a6e4f7f9b02dc60be8ffaff60f8
Nguyễn Chí Cường <cuong.nc.0804@gmail.com>
2026-05-03 14:41:25 +0700

    Implement logging for file processing in main.py
    Added logging to track file processing and errors.

--- main.py
+++ main.py
@@ -3,34 +3,42 @@ import shutil
 import tempfile
 from fastapi import FastAPI, UploadFile, File
 from faster_whisper import WhisperModel
-
+import logging
 app = FastAPI()
+# import uvicorn
 
-# Khởi tạo model (nên để bên ngoài để load 1 lần duy nhất)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 MODEL_PATH = "./models/latest" 
+# MODEL_PATH = "D:\Project\Whisper_Faster_Model"
 model = WhisperModel(MODEL_PATH, device="cuda", compute_type="float16")
 
 @app.post("/transcribe")
 async def transcribe_api(file: UploadFile = File(...)):
     suffix = os.path.splitext(file.filename)[1]
-    
+    logger.info(f"--- Bắt đầu xử lý file: {file.filename} ---")
     with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
         shutil.copyfileobj(file.file, tmp)
         temp_path = tmp.name
 
     try:
+        logger.info(f"Đang tạo file tạm cho {file.filename}")
         segments, _ = model.transcribe(temp_path, language="vi", beam_size=5)
         
         text = " ".join([s.text for s in segments]).strip()
-        
+        logger.info("Chuyển đổi âm thanh thành văn bản thành công.")
         return {
             "filename": file.filename, 
             "transcription": text
         }
     
     except Exception as e:
+        logger.error(f"Lỗi xảy ra khi xử lý file {file.filename}: {str(e)}")
         return {"error": str(e)}
     
     finally:
         if os.path.exists(temp_path):
             os.remove(temp_path)
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8080)
